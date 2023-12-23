7c857db0442c09c62647697ad9aee9be62a306a2
baalaandaa <kirill@baland.in>
2023-12-24 01:31:00 +0300

    Driver kafka WIP

---
+++ internal/driver/model/kafka.go
@@ -0,0 +1,119 @@
+package model
+
+import (
+	"github.com/gofrs/uuid"
+	"time"
+)
+
+type KafkaMessage struct {
+	ID              string      `json:"id"`
+	Source          string      `json:"source"`
+	Type            string      `json:"type"`
+	DataContentType string      `json:"datacontenttype"`
+	Time            string      `json:"time"`
+	Data            interface{} `json:"data"`
+}
+
+type CreatedTripEvent struct {
+	ID              string `json:"id"`
+	Source          string `json:"source"`
+	Type            string `json:"type"`
+	DataContentType string `json:"datacontenttype"`
+	Time            string `json:"time"`
+	Data            *Trip  `json:"data"`
+}
+
+func NewCreatedTripEvent(data *Trip) *CreatedTripEvent {
+	id, _ := uuid.NewV4()
+	return &CreatedTripEvent{
+		ID:              id.String(),
+		Source:          "/driver",
+		Type:            "trip.event.create",
+		DataContentType: "application/json",
+		Time:            time.Now().Format(time.RFC3339),
+		Data:            data,
+	}
+}
+
+type AcceptTripPayload struct {
+	TripID   string `json:"trip_id"`
+	DriverID string `json:"driver_id"`
+}
+
+type AcceptTripCommand struct {
+	ID              string            `json:"id"`
+	Source          string            `json:"source"`
+	Type            string            `json:"type"`
+	DataContentType string            `json:"datacontenttype"`
+	Time            string            `json:"time"`
+	Data            AcceptTripPayload `json:"data"`
+}
+
+func NewAcceptTripCommand(tripID, driverID string) *AcceptTripCommand {
+	id, _ := uuid.NewV4()
+	return &AcceptTripCommand{
+		ID:              id.String(),
+		Source:          "/driver",
+		Type:            "trip.command.accept",
+		DataContentType: "application/json",
+		Time:            time.Now().Format(time.RFC3339),
+		Data: AcceptTripPayload{
+			TripID:   tripID,
+			DriverID: driverID,
+		},
+	}
+}
+
+type EndTripPayload struct {
+	TripID string `json:"trip_id"`
+}
+
+type EndTripCommand struct {
+	ID              string         `json:"id"`
+	Source          string         `json:"source"`
+	Type            string         `json:"type"`
+	DataContentType string         `json:"datacontenttype"`
+	Time            string         `json:"time"`
+	Data            EndTripPayload `json:"data"`
+}
+
+func NewEndTripCommand(tripID string) *EndTripCommand {
+	id, _ := uuid.NewV4()
+	return &EndTripCommand{
+		ID:              id.String(),
+		Source:          "/driver",
+		Type:            "trip.command.end",
+		DataContentType: "application/json",
+		Time:            time.Now().Format(time.RFC3339),
+		Data: EndTripPayload{
+			TripID: tripID,
+		},
+	}
+}
+
+type StartTripPayload struct {
+	TripID string `json:"trip_id"`
+}
+
+type StartTripCommand struct {
+	ID              string           `json:"id"`
+	Source          string           `json:"source"`
+	Type            string           `json:"type"`
+	DataContentType string           `json:"datacontenttype"`
+	Time            string           `json:"time"`
+	Data            StartTripPayload `json:"data"`
+}
+
+func NewStartTripCommand(tripID string) *StartTripCommand {
+	id, _ := uuid.NewV4()
+	return &StartTripCommand{
+		ID:              id.String(),
+		Source:          "/driver",
+		Type:            "trip.command.end",
+		DataContentType: "application/json",
+		Time:            time.Now().Format(time.RFC3339),
+		Data: StartTripPayload{
+			TripID: tripID,
+		},
+	}
+}
