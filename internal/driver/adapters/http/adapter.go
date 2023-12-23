68b0233d7bfa6e3301531eeea7c85f60fe3e54cf
Roman Evdokimov <rnevdokimov.42@gmail.com>
2023-12-23 04:42:10 +0000

    add handlers to http adapter

--- internal/driver/adapters/http/adapter.go
+++ internal/driver/adapters/http/adapter.go
@@ -32,7 +32,12 @@ func (a adapter) Serve() error {
 
 	apiRouter := chi.NewRouter()
 
-	// apiRouter.Get("/some_method", http.HandlerFunc(a.driverHandler.SomeMethod))
+	apiRouter.Get("/trips", http.HandlerFunc(a.driverHandler.ListTrips))
+	apiRouter.Get("/trips/{trip_id}", http.HandlerFunc(a.driverHandler.GetTrip))
+	apiRouter.Post("/trips/{trip_id}/cancel", http.HandlerFunc(a.driverHandler.CancelTrip))
+	apiRouter.Post("/trips/{trip_id}/accept", http.HandlerFunc(a.driverHandler.AcceptTrip))
+	apiRouter.Post("/trips/{trip_id}/start", http.HandlerFunc(a.driverHandler.StartTrip))
+	apiRouter.Post("/trips/{trip_id}/end", http.HandlerFunc(a.driverHandler.EndTrip))
 
 	r.Mount(a.config.BasePath, apiRouter)
 	a.server = &http.Server{Addr: a.config.ServeAddress, Handler: r}
